8185a103a55a86717dccad699f5ffb1265db48fd
Rahul Reekhadi <rahulreekhadi87@gmail.com>
2025-07-18 01:43:02 +0530

    "User model"

---
+++ backend/models/users.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+
+const userSchema = new mongoose.Schema({
+    fullName:{
+        firstName:{
+            type:String,
+            requried:true,
+            minlength:[3,"First name must be atleast 3 characters long"]
+        },
+        lastName:{
+            type:String,
+            minlength:[3,"Last name must be atleast 3 characters long"]
+        }
+    },
+    email:{
+        type:String,
+        requried:true,
+        unique:true,
+        minlength:[5,"Email must be atleast 5 characters long"]
+    },
+    password:{
+        type:String,
+        required:true,
+        select:false,
+    },
+    socketId:{
+        type:String
+    }
+})
+
+userSchema.method.generateAuthToken = function(){
+    const token = jwt.sign({_id:this.id},process.env.JWT_SECRET)
+    return token;
+}
+
+userSchema.method.comparePassword = async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+
+userSchema.method.hashPassword =async function(password){
+    return await bcrypt.hash(password,10);
+}
+
+
+const User = mongoose.model("user",userSchema);
+export default User;
